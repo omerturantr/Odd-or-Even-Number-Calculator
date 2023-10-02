cddcf2ce9a2b3cc3e30f500276643ea5d30ff5ca
Omer Faruk TURAN <faruktrn25@gmail.com>
2023-10-02 15:17:57 +0300

    EvenOrOdd
    JAVA Program that checks whether the number received from the user is EVEN or ODD.

---
+++ EvenOrOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;     // Activing 'Scanner' Library
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+
+        int number;     // Declaring the Variable
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an Integer: ");   number = input.nextInt();     // Receiving Input from the user.
+
+        if(number % 2 == 0) {    
+            System.out.print(number + " is an EVEN Integer.");     // If the number is exactly divisible by 2, print this message.
+        }
+        else {
+            System.out.print(number + " is an ODD Integer.");     // If the number is not exactly divisible by 2, print this message.
+        }
+    }
+}
